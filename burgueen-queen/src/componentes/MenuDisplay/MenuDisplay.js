ed08fa3d30e97ae381d1eb64f2f56271ac74a136
danicagarcia16 <danicagarcia1613@gmail.com>
2023-03-05 09:51:17 -0500

    creación del componente menudisplay para los botenes del menu

---
+++ burgueen-queen/src/componentes/MenuDisplay/MenuDisplay.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const MenuDisplay = props => {
+  // console.log(props.menu);
+
+  if (props.menu === null) {
+    return (
+      <div>
+        <p>Elige un menú para ver los items disponibles</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="menu">
+      {props.menu.map((item, index) => (
+        <button
+          className="option-buttons"
+          key={index}
+          price={item.price}
+          name={item.name}
+          onClick={() => props.onClick(item.price, item.name)}
+        >
+          {item.name.toUpperCase()+"\n $" + item.price}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default MenuDisplay;
